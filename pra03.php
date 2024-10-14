502fad9b49f1ca9f3c709184a7e29e2f5fb1bf07
mackliu <macklun@gmail.com>
2024-10-14 08:39:56 +0800

    完成挖空矩形的製作

--- pra03.php
+++ pra03.php
@@ -150,7 +150,21 @@ for($i=0;$i<21;$i++){
 }
 ?>
 <h2>矩形練習</h2>
- <?php 
+<?php 
+for($i=0;$i<5;$i++){
+    for($j=0;$j<5;$j++){
+        if($i==0 || $i==4){
+            echo "*";
+        }else if($j==0 ||$j==4){
+            echo "*";
+        }else{
+            echo "&nbsp;";
+        }
+    }
+    echo "<br>";
+}
+
+
 
 ?>
 
