3c45579c948770a61d8f2c44df54577a120c8de6
mackliu <macklun@gmail.com>
2024-10-22 11:29:20 +0800

    產生陣列的多種方式示範

--- array.php
+++ array.php
@@ -263,7 +263,73 @@ foreach($nine as $idx => $n){
     }
 }
 echo "</table>";
+echo "<hr>";
+$nine2=[];
 
+for($i=1;$i<=9;$i++){
+    for($j=1;$j<=9;$j++){
+        $nine2["$i x $j"]=$i*$j;
+    }    
+}
+
+echo "<pre>";
+print_r($nine2);
+echo "</pre>";
+
+echo "<hr>";
+
+$counter=0;
+foreach($nine2 as $key => $n){
+    $counter++;
+    if($counter==9){
+        echo $key  . " = ".$n ."<br>";
+        $counter=0;
+    }else{
+        echo $key  . " = ".$n .", ";
+
+    }
+}
+echo "<hr>";
+
+echo "<table>";
+$counter=0;
+foreach($nine2 as $n){
+    $counter++;
+    if($counter==9){
+        echo "<td>$n</td></tr>";
+        $counter=0;
+    }else if($counter==1){
+        echo "<tr><td>$n</td>";
+
+    }else{
+        echo "<td>$n</td>";
+    }
+}
+echo "</table>";
+echo "<hr>";
+echo "<hr>";
+$nine3=[];
+
+for($i=1;$i<=9;$i++){
+    for($j=1;$j<=9;$j++){
+        $nine3[]=['formula' => "$i x $j",'value'=>$i*$j];
+    }    
+}
+
+echo "<pre>";
+print_r($nine3);
+echo "</pre>";
+
+echo "<hr>";
+
+foreach($nine3 as $idx =>$item){
+    if($idx%9==8){
+        echo $item['formula'] . " = ".$item['value']. "<br>";
+    }else{
+        echo $item['formula'] . " = ".$item['value'];
+
+    }
+}
 
 /* echo "<table>";
 for($i=1;$i<=9;$i++){
