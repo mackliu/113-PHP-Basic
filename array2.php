438ec1cabc8839d5a631a10e64e16d839fc2a834
mackliu <macklun@gmail.com>
2024-10-22 16:33:26 +0800

    反轉陣列元素

--- array2.php
+++ array2.php
@@ -95,8 +95,34 @@ echo $sl[($year-4)%60];
 
 
 ?>
+<h2>請設計一支程式，在不產生新陣列的狀況下，將一個陣列的元素順序反轉(利用迴圈)</h2>
+
+<ul>
+    <li>例：$a=[2,4,6,1,8] 反轉後 $a=[8,1,6,4,2]</li>
+</ul>
+<?php
+
+$a=[2,4,6,1,8,5];
+echo "<pre>";
+print_r($a);
+echo "</pre>";
+for($i=0;$i<floor(count($a)/2);$i++){
+    $tmp=$a[$i];
+    $a[$i]=$a[count($a)-1-$i];
+    $a[count($a)-1-$i]=$tmp;
+    }
+echo "<br>";
+echo "<pre>";
+print_r($a);
+echo "</pre>";
+echo "<hr>";
+echo "<pre>";
+print_r(array_reverse($a));
+echo "</pre>";
 
 
+
+?>
 
 
 <p>&nbsp;</p>
