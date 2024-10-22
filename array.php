de50a604894547d774690838f3eaa2e0a439713e
mackliu <macklun@gmail.com>
2024-10-22 09:51:50 +0800

    使用陣列來儲存九九乘法表的練習

--- array.php
+++ array.php
@@ -194,6 +194,61 @@ echo "</pre>";
     }
     ?>
 </table>
+<h2>利用程式來產生陣列</h2>
+<!-- ul>li* -->
+<ul>
+    <li>以迴圈的方式產生一個九九乘法表</li>
+    <li>將九九乘法表的每個項目以字串型式存入陣列中</li>
+    <li>再以迴圈方式將陣列內容印出</li>
+</ul>
+
+<?php
+$nine=[];
+
+for($i=1;$i<=9;$i++){
+    for($j=1;$j<=9;$j++){
+        $nine[]="$i x $j = ".($i*$j);
+    }    
+}
+/* echo "<pre>";
+print_r($nine);
+echo "</pre>"; */
+$counter=0;
+foreach($nine as $idx => $n){
+    $counter++;
+    if($counter==9){
+        echo $n . "<br>";
+        $counter=0;
+    }else{
+        echo $n . ", ";
+
+    }
+
+}
+echo "<hr>";
+foreach($nine as $idx => $n){
+    if($idx%9==8){
+        echo $n . "<br>";
+    }else{
+        echo $n . ", ";
+    }
+}
+
+
+/* echo "<table>";
+for($i=1;$i<=9;$i++){
+    echo "<tr>";
+    for($j=1;$j<=9;$j++){
+        echo "<td>";
+        echo "$i x $j = ";
+        echo $i*$j;
+        echo "</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>"; */
+
+?>
 
 <p>&nbsp;</p>
 <p>&nbsp;</p>
