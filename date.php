ac7cf6422197d7e98e5fab69a9de17c8e7953b2d
mackliu <macklun@gmail.com>
2024-10-24 14:13:10 +0800

    日期時間練習

--- date.php
+++ date.php
@@ -50,6 +50,67 @@ echo "距下次生日還有:".$days ."天";
 echo "<br>";
 ?>
 
+<h1>利用date()函式的格式化參數，完成以下的日期格式呈現</h1>
+<ul>
+    <li>2021/10/05</li>
+    <li>10月5日 Tuesday</li>
+    <li>2021-10-5 12:9:5</li>
+    <li>2021-10-5 12:09:05</li>
+    <li>今天是西元2021年10月5日 上班日(或假日)</li>
+</ul>
+<?php 
+$weekString=[
+        'Monday'=>['min'=>'一','short'=>'周一','fulltext'=>'星期一'],
+        'Tuesday'=>['min'=>'二','short'=>'周二','fulltext'=>'星期二'],
+        'Wednesday'=>['min'=>'三','short'=>'周三','fulltext'=>'星期三'],
+        'Thursday'=>['min'=>'四','short'=>'周四','fulltext'=>'星期四'],
+        'Friday'=>['min'=>'五','short'=>'周五','fulltext'=>'星期五'],
+        'Saturday'=>['min'=>'六','short'=>'周六','fulltext'=>'星期六'],
+        'Sunday'=>['min'=>'七','short'=>'周日','fulltext'=>'星期日']
+        ];
+echo date("Y年m月d日");
+echo "<br>";
+echo date("m月d日 l");
+echo "<br>";
+echo date("m月d日").$weekString[date("l")]['min'];
+echo "<br>";
+echo date("m月d日").$weekString[date("l")]['short'];
+echo "<br>";
+echo date("m月d日").$weekString[date("l")]['fulltext'];
+echo "<br>";
+echo "今天是西元".date("Y年m月d日");
+if(date("N")>5){
+    echo "假日";
+}else{
+    echo "上班日";
+}
+echo "<br>";
+
+?>
+<h1>
+    利用迴圈來計算連續五個周一的日期
+    例:
+</h1>
+
+<ul>
+    <li>2021-10-04 星期一</li>
+    <li>2021-10-11 星期一</li>
+    <li>2021-10-18 星期一</li>
+    <li>2021-10-25 星期一</li>
+    <li>2021-11-01 星期一</li>
+</ul>
+
+<?php 
+for($i=0;$i<5;$i++){
+    $timestamp=strtotime("+$i weeks".date("Y-m-d"));
+    echo date("Y-m-d",$timestamp);
+    echo "&nbsp;";
+    echo $weekString[date("l")]['fulltext'];
+    echo "<br>";
+}
+
+?>
+
 
 <p>&nbsp;</p>
 <p>&nbsp;</p>
